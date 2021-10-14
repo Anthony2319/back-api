19a888e840bc58cc5b2496980874787430cbf91a
anthony <anthony.moreel62@gmail.com>
2021-10-14 11:18:27 +0200

    modif index

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Entity\Project;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +21,10 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+            $routeBuilder = $this->get(AdminUrlGenerator::class);
+            $url = $routeBuilder->setController(ProjectCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
